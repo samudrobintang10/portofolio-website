5d72d86f722b35fb6339da70cce3a4c8a5b9f65d
samudrobintang10 <samudrobintang10@gmail.com>
2022-07-22 14:18:37 +0700

    change smallest font size

--- components/elements/Button/index.js
+++ components/elements/Button/index.js
@@ -1,7 +1,7 @@
 export default function Button({placeholder}) {
   return (
     <button className=" rounded-lg bg-primary-brown border border-black px-6 py-2 w-fit">
-      <p className=" text-black font-nunito text-xs lg:text-base">{placeholder}</p>
+      <p className=" text-black font-nunito text-sm lg:text-base">{placeholder}</p>
     </button>
   );
 }
